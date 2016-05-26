a45bc3b2bd5504597ebc27944f05cfb96c425279
panha_AlienZ <song.sooin168@gmail.com>
2016-05-26 23:02:52 +0700

    Sharing

---
+++ XCalculator/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  XCalculator
+//
+//  Created by Meas Panha on 5/12/16.
+//  Copyright © 2016 Meas Panha. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController{
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+   
+ 
+    
+    
+   }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  
+  
+  
+}
+
